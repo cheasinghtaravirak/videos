9c3592f17e40cb5846124eadee9f3487fe42e4ca
Singhtaravirak Chea <singhtaravirak@gmail.com>
2019-01-08 16:48:08 +0700

    making youtube api request

---
+++ src/api/youtube.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const KEY = 'AIzaSyApqEPYF0BxYn4SEpVlDvvLOPiNiljBbT8';
+// custom client request
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    key: KEY,
+    part: 'snippet',
+    maxResults: '1',
+    type: ''
+  }
+});
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import youtube from '../api/youtube';
 //App is a class based component b/c it handles application's state
 class App extends React.Component {
 
   onSearchSubmit = term => {
-    console.log(term); 
+    youtube.get('/search', {
+      params: {
+        q: term,
+      }
+    });
   }
 
   render() {
